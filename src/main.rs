b82716d8b4e733cac4ecd45bbfe16cb6ce3d9001
danielrachi <hi@danielrachi.com>
2025-12-23 08:42:07 -0500

    Refactor out of stringy API
    Use structs and enums for a more idiomatic logic, instead of strings.
    
    The string-based logic was getting harder to extend.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,52 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Command {
+    Exit,
+    Echo,
+}
+
+impl From<&str> for Command {
+    fn from(value: &str) -> Self {
+        match value {
+            "exit" => Command::Exit,
+            "echo" => Command::Echo,
+            _ => panic!("Unknown command"),
+        }
+    }
+}
+
+struct Input<'a> {
+    command: Command,
+    args: Vec<&'a str>,
+}
+
+impl Input<'static> {
+    fn new(buf: &mut str) -> Input<'_> {
+        let mut buf_vec = buf.split_whitespace();
+        Input {
+            command: buf_vec.nth(0).unwrap().into(),
+            args: buf_vec.collect(),
+        }
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
 
-        let mut input = input.split_whitespace();
-        let command = input.next().unwrap();
-        match command {
-            "exit" => break,
-            "echo" => {
-                for arg in input {
+        let input = Input::new(&mut buf);
+        match input.command {
+            Command::Exit => break,
+            Command::Echo => {
+                for arg in input.args {
                     print!("{} ", arg);
                 }
                 println!();
-            }
-            _ => {
-                println!("{}: command not found", command)
             }
         }
     }
