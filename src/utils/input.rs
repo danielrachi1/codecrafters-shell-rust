72052cc90a15280a75dc98ea6abdf57d75c11890
danielrachi <hi@danielrachi.com>
2025-12-29 12:43:11 -0500

    Implement support for quoting with single quotes
    Change the argument parsing logic to handle single quotes.
    Single quotes (') disable all special meaning for characters enclosed
    within them. Every character between single quotes is treated literally.

--- src/utils/input.rs
+++ src/utils/input.rs
@@ -8,13 +8,10 @@ pub struct Input {
 
 impl Input {
     pub fn new(buf: &mut str) -> Result<Input, InputError> {
-        let mut buf_vec = buf.split_whitespace();
-        let command = buf_vec.next().ok_or(InputError::EmptyCommand)?.into();
-        Input {
-            command,
-            args: buf_vec.map(|arg| arg.to_string()).collect(),
-        }
-        .check_args()
+        let buf_tup = buf.split_once(" ").ok_or(InputError::EmptyCommand)?;
+        let command = buf_tup.0.into();
+        let args = split_args(buf_tup.1);
+        Input { command, args }.check_args()
     }
 
     fn check_args(self) -> Result<Self, InputError> {
@@ -29,4 +26,19 @@ impl Input {
             _ => Ok(self),
         }
     }
+}
+
+fn split_args(args: &str) -> Vec<String> {
+    let mut quote_flag = false;
+    args.split(|c: char| {
+        if c == '\'' {
+            quote_flag = !quote_flag;
+            false
+        } else {
+            c.is_whitespace() & !quote_flag
+        }
+    })
+    .filter(|arg| !arg.is_empty())
+    .map(|arg| arg.replace("'", ""))
+    .collect()
 }
