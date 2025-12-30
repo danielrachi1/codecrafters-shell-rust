2ed2e73a83a84a7660e685c8ee71182a6a4f7fdf
danielrachi <hi@danielrachi.com>
2025-12-29 20:45:16 -0500

    Handle executable names with quotes

--- src/utils/input.rs
+++ src/utils/input.rs
@@ -9,10 +9,13 @@ pub struct Input {
 
 impl Input {
     pub fn new(buf: String) -> Result<Input, InputError> {
-        let buf_tup = buf.split_once(" ").unwrap_or((buf.trim(), ""));
-        let command = buf_tup.0.into();
-        let args = ArgumentParser::new(buf_tup.1.trim().to_string()).parse();
-        Input { command, args }.check_args()
+        let parsed_input = ArgumentParser::new(buf.trim().to_string()).parse();
+        let (command, args) = parsed_input.split_at(1);
+        Input {
+            command: command[0].as_str().into(),
+            args: args.to_vec(),
+        }
+        .check_args()
     }
 
     fn check_args(self) -> Result<Self, InputError> {
